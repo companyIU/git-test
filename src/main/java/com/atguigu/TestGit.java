fad79a260f281af7a8cf993179aa67c3a72569e4
companyIU <3215602412@qq.com>
2022-12-06 19:29:15 +0800

    第一次修改

---
+++ src/main/java/com/atguigu/TestGit.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author wbstart
+ * @create 2022-12-06 17:23
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello001");
+    }
+
+}
